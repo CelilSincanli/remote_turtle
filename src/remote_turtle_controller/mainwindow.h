12dc757df116565f7d0cdc286461ff32a7abfaaa
“celilsincanli1” <celil@adastec.com>
2023-08-18 10:45:17 +0300

    feat(remote_turtle): added widgets

--- src/remote_turtle_controller/mainwindow.h
+++ src/remote_turtle_controller/mainwindow.h
@@ -15,6 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_up_button_clicked();
+
+    void on_left_button_clicked();
+
+    void on_right_button_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
